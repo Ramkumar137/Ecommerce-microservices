8219f8dead3d6c9b95a14d6fcb007208927710d3
Ramkumar J <ramkumar.j@idp.com>
2026-08-06 23:27:10 +0530

    update order service

--- order-service/config/settings.py
+++ order-service/config/settings.py
@@ -140,8 +140,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://localhost:4173",
-    "http://127.0.0.1:4173",
+    "https://d1o0yv23okm5t4.cloudfront.net",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
 ]
